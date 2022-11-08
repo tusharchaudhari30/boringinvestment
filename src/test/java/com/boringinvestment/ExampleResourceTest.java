f82bb069a75dbca2d68f95816908eb0b06d6d041
STARK <chaudharitushar2077@gmail.com>
2022-11-08 04:03:28 -0800

    auth added

--- src/test/java/com/boringinvestment/ExampleResourceTest.java
+++ src/test/java/com/boringinvestment/ExampleResourceTest.java
@@ -11,11 +11,7 @@ public class ExampleResourceTest {
 
     @Test
     public void testHelloEndpoint() {
-        given()
-                .when().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(is("Hello RESTEasy"));
+
     }
 
 }
