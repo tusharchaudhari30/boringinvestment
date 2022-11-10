f08d7765250599f0d62b54042a25d2df50649f71
STARK <chaudharitushar2077@gmail.com>
2022-11-10 05:44:34 -0800

    transaction model changes

--- personalfin/src/component/Client/HomeClient.js
+++ personalfin/src/component/Client/HomeClient.js
@@ -2,13 +2,13 @@ class HomeClient {
   static url = "";
 
   static async Portfolio() {
-    var myHeaders = new Headers();
+    const myHeaders = new Headers();
     myHeaders.append(
       "Authorization",
       "Bearer " + localStorage.getItem("token")
     );
 
-    var requestOptions = {
+    const requestOptions = {
       method: "GET",
       headers: myHeaders,
       redirect: "follow",
@@ -25,7 +25,7 @@ class HomeClient {
       "Authorization",
       "Bearer " + localStorage.getItem("token")
     );
-    var requestOptions = {
+    const requestOptions = {
       method: "GET",
       headers: myHeaders,
       redirect: "follow",
@@ -35,5 +35,53 @@ class HomeClient {
       (response) => response.json()
     );
   }
+  static async saveTransaction(assetName, ticker, amount, average, date) {
+    const myHeaders = new Headers();
+    myHeaders.append(
+      "Authorization",
+      "Bearer " + localStorage.getItem("token")
+    );
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      assetName: assetName,
+      ticker: ticker,
+      average: average,
+      quantity: amount,
+      transactionDate: date,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(this.url + "/transaction", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  }
+
+  static async assetList(assetName) {
+    const myHeaders = new Headers();
+    myHeaders.append(
+      "Authorization",
+      "Bearer " + localStorage.getItem("token")
+    );
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    return fetch(
+      this.url + "/asset/search?keyword=" + assetName,
+      requestOptions
+    ).then((response) => response.json());
+  }
 }
+
 export default HomeClient;
